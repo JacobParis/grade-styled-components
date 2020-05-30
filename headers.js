83aae3b41389d417c860dfb1daf349b88b84f43a
Jacob Paris <jacob@jacobpariseau.com>
2020-05-30 14:07:53 -0400

    Change header to dark overlay

--- headers.js
+++ headers.js
@@ -25,6 +25,7 @@ export const Header = styled.div`
         background-color: ${getColorFromProps};
         border-radius: ${({float}) => float ? Theme.Layout.Corners : "0"};
         z-index: -1;
+        opacity: 0.4
     }
 `;
 
